2eb3c7db2ffb0601ab4ba9114b82cb5af035281e
AdiWeit <Weitkemper.Adrian@web.de>
2022-12-23 17:16:48 +0100

    show sensitivity (as text)

--- Assets/Scripts/UI/settings/toggleSensibility.cs
+++ Assets/Scripts/UI/settings/toggleSensibility.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class toggleSensibility : MonoBehaviour
 {
+  public GameObject sensText;
   public void sensitivityChanged() {
     GameObject.Find("movingCube").GetComponent<movePlane>().sensitivityChanged(this);
+    sensText.gameObject.GetComponent<TMPro.TextMeshProUGUI>().text = ((Mathf.Round((this.GetComponent<Slider>().value) * 100)) / 100.0).ToString();
   }
 }
